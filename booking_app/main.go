6f492ed7afe90ec8cfcd42aab8e0309e2f2b53fc
ENISSAY39 <yassinebsofien@gmail.com>
2025-10-26 00:17:20 +0700

    If else statment

--- booking_app/main.go
+++ booking_app/main.go
@@ -37,6 +37,11 @@ func main() {
 		fmt.Println("Enter your number of tickets:")
 		fmt.Scan(&UserTickets)
 
+		if UserTickets > int(remainingTickets) {
+			fmt.Printf("We only have %v tickets remaining, so you can't book %v tickets\n", remainingTickets, UserTickets)
+			continue
+		}
+
 		remainingTickets = remainingTickets - uint(UserTickets)
 
 		// Store user data
@@ -55,6 +60,12 @@ func main() {
 		}
 
 		fmt.Printf("The first names of bookings are: %v\n", firstNames)
+
+		if remainingTickets == 0 {
+			// end program
+			fmt.Println("Our conference is booked out. Come back next year.")
+			break
+		}
 
 	}
 
