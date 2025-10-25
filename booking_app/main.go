9b07893277bd59f4492ae3b35f53078d737a6988
ENISSAY39 <yassinebsofien@gmail.com>
2025-10-25 19:46:28 +0700

    Basic Loop

--- booking_app/main.go
+++ booking_app/main.go
@@ -12,37 +12,41 @@ func main() {
 	fmt.Printf("We have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var firstName string
-	var lastName string
-	var email string
-	var UserTickets int
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var UserTickets int
 
-	// Ask user first and last name
+		// Ask user first and last name
 
-	fmt.Println("Enter your first name and last name:")
-	fmt.Scan(&firstName)
-	fmt.Scan(&lastName)
+		fmt.Println("Enter your first name and last name:")
+		fmt.Scan(&firstName)
+		fmt.Scan(&lastName)
 
-	// Ask user email address
+		// Ask user email address
 
-	fmt.Println("Enter your email address:")
-	fmt.Scan(&email)
+		fmt.Println("Enter your email address:")
+		fmt.Scan(&email)
 
-	// Ask user number of tickets
+		// Ask user number of tickets
 
-	fmt.Println("Enter your number of tickets:")
-	fmt.Scan(&UserTickets)
+		fmt.Println("Enter your number of tickets:")
+		fmt.Scan(&UserTickets)
 
-	remainingTickets = remainingTickets - uint(UserTickets)
+		remainingTickets = remainingTickets - uint(UserTickets)
 
-	// Store user data
+		// Store user data
 
-	bookings = append(bookings, firstName+" "+lastName)
+		bookings = append(bookings, firstName+" "+lastName)
 
-	// Print user data
+		// Print user data
 
-	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation e-mail at %v															", firstName, lastName, UserTickets, email)
-	fmt.Printf(" %v tickets remaining for %v\n", remainingTickets, conferenceName)
+		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation e-mail at %v															", firstName, lastName, UserTickets, email)
+		fmt.Printf(" %v tickets remaining for %v\n", remainingTickets, conferenceName)
 
-	fmt.Printf("These are all our bookings: %v\n", bookings)
+		fmt.Printf("These are all our bookings: %v\n", bookings)
+
+	}
+
 }
