cd38a6a864556cdf562835528aff2c6ce97b3994
ENISSAY39 <yassinebsofien@gmail.com>
2025-10-29 10:45:19 +0700

    Maping

--- booking-app/main.go
+++ booking-app/main.go
@@ -3,14 +3,14 @@ package main
 import (
 	"booking-app/helper"
 	"fmt"
-	"strings"
+	"strconv"
 )
 
 const conferenceTickets int = 50
 
 var conferenceName = "Go Conference"
 var remainingTickets uint = 50
-var bookings = []string{}
+var bookings = make([]map[string]string, 0)
 
 func main() {
 	greetUsers()
@@ -61,8 +61,7 @@ func greetUsers() {
 func getFirstNames() []string {
 	firstNames := []string{}
 	for _, booking := range bookings {
-		var names = strings.Fields(booking)
-		firstNames = append(firstNames, names[0])
+		firstNames = append(firstNames, booking["firstName"])
 	}
 	return firstNames
 }
@@ -97,9 +96,19 @@ func getUserInput() (string, string, string, uint) {
 func bookTicket(UserTickets uint, firstName string, lastName string, email string) ([]string, uint) {
 	remainingTickets = remainingTickets - uint(UserTickets)
 
-	bookings = append(bookings, firstName+" "+lastName)
+	// create a map for a user
+	var userData = make(map[string]string)
+	userData["firstName"] = firstName
+	userData["lastName"] = lastName
+	userData["email"] = email
+	userData["NumberOfTickets"] = strconv.FormatUint(uint64(UserTickets), 10)
+
+	bookings = append(bookings, userData)
+	fmt.Printf("List of bookings is %v\n", bookings)
 
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation e-mail at %v", firstName, lastName, UserTickets, email)
 	fmt.Printf(" %v tickets remaining for %v\n", remainingTickets, conferenceName)
-	return bookings, remainingTickets
+
+	return getFirstNames(), remainingTickets
+
 }
